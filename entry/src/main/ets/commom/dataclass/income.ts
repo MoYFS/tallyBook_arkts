596bb5028b577d7274ae558ab0692e045c08605e
莫语浮生 <zhangjiarui2018@outlook.com>
2023-11-06 23:14:52 +0800

    课设2023年11月06日23点14分修改

---
+++ entry/src/main/ets/commom/dataclass/income.ts
@@ -0,0 +1,16 @@
+export class income{
+  type:string
+  category:string//种类
+  money:number//金额
+  remarks:string//备注
+  time:string
+
+  constructor(cate:string,money:number,remarks?:string) {
+    this.type='收入'
+    this.category=cate
+    this.money=money
+    this.remarks=remarks?remarks:''
+    let temp=new Date
+    this.time=temp.getFullYear().toString()+'-'+temp.getMonth().toString()+'-'+temp.getDate().toString()+' '+temp.getHours().toString()+':'+temp.getMinutes()+':'+temp.getSeconds().toString()
+  }
+}
