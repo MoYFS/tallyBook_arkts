6f43c8d4381262febfb97620ff6c1d3f5f0ff2e9
moyufusheng <2331010886@qq.com>
2023-12-10 15:15:36 +0800

    2023年12月10日15点14分关系型数据库加入成功，课设项目基本完成

--- entry/src/main/ets/commom/database/dataStorage.ts
+++ entry/src/main/ets/commom/database/dataStorage.ts
@@ -1,23 +1,108 @@
 import relationalStore from '@ohos.data.relationalStore'
+import common from '@ohos.app.ability.common';
 import hilog from '@ohos.hilog';
 
 export class RdbStoreUtil{
-  creaftRdbStoreUtil(context){
+  private rdbStore: relationalStore.RdbStore | null = null;
+  private tableName: string;
+  private sqlCreateTable: string;
+  private columns: Array<string>;
+
+  constructor() {
+    this.tableName = 'INOUTLIST';
+    this.sqlCreateTable = 'CREATE TABLE IF NOT EXISTS INOUTLIST (ID INTEGER PRIMARY KEY , TYPEID INTEGER,CATEID INTEGER,CATE TEXT,MONEY REAL,REMARKS TEXT,TIME TEXT)';
+    this.columns = ["ID","TYPEID","CATEID","CATE","MONEY","REMARKS","TIME"];
+  }
+
+  getRdbStore(context:common.BaseContext,callback: Function = () => {
+  }) {
+    if (!callback || typeof callback === 'undefined' || callback === undefined) {
+      return;
+    }
+    if (this.rdbStore !== null) {
+      callback();
+      return
+    }
     const Store_Config={
       name: "moneyData.db",
       securityLevel: relationalStore.SecurityLevel.S2
     };
-    hilog.info(0x0000,'testTag','成功')
-    globalThis.getRdbStoreUtil=(()=>{
-      let RdbStore:Promise<relationalStore.RdbStore>=relationalStore.getRdbStore(context,Store_Config);
-      return RdbStore;
-    })
+    relationalStore.getRdbStore(context, Store_Config, (err, rdb) => {
+      if (err) {
+        return;
+      }
+      this.rdbStore = rdb;
+      this.rdbStore.executeSql(this.sqlCreateTable);
+      callback();
+    });
   }
-  saveDefaultRdbStoreUtil(SQL_CREATE_TABLE:string){
-    globalThis.getRdbStoreUtil().then(async(store)=>{
-      store.executeSql(SQL_CREATE_TABLE);
-      hilog.info(0x0000,"testTag",'创建表')
-    })
+
+  insertData(data: relationalStore.ValuesBucket, callback: Function = () => {
+  }) {
+    if (!callback || typeof callback === 'undefined' || callback === undefined) {
+      return;
+    }
+    let resFlag: boolean = false;
+    const valueBucket: relationalStore.ValuesBucket = data;
+    if (this.rdbStore) {
+      this.rdbStore.insert(this.tableName, valueBucket, (err, ret) => {
+        if (err) {
+          callback(resFlag);
+          return;
+        }
+        callback(ret);
+      });
+    }
+  }
+
+  deleteData(predicates: relationalStore.RdbPredicates, callback: Function = () => {
+  }) {
+    if (!callback || typeof callback === 'undefined' || callback === undefined) {
+      return;
+    }
+    let resFlag: boolean = false;
+    if (this.rdbStore) {
+      this.rdbStore.delete(predicates, (err, ret) => {
+        if (err) {
+          callback(resFlag);
+          return;
+        }
+        callback(!resFlag);
+      });
+    }
+  }
+
+  updateData(predicates: relationalStore.RdbPredicates, data: relationalStore.ValuesBucket, callback: Function = () => {
+  }) {
+    if (!callback || typeof callback === 'undefined' || callback === undefined) {
+      return;
+    }
+    let resFlag: boolean = false;
+    const valueBucket: relationalStore.ValuesBucket = data;
+    if (this.rdbStore) {
+      this.rdbStore.update(valueBucket, predicates, (err, ret) => {
+        if (err) {
+          callback(resFlag);
+          return;
+        }
+        callback(!resFlag);
+      });
+    }
+  }
+
+  query(predicates: relationalStore.RdbPredicates, callback: Function = () => {
+  }) {
+    if (!callback || typeof callback === 'undefined' || callback === undefined) {
+      return;
+    }
+    if (this.rdbStore) {
+      this.rdbStore.query(predicates, this.columns, (err, resultSet) => {
+        if (err) {
+          return;
+        }
+        callback(resultSet);
+        resultSet.close();
+      });
+    }
   }
 }
-export default new RdbStoreUtil();
